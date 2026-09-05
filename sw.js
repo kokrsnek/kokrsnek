5460e45875ec55ba63eeed814b7a5c193dc2a3ce
kokrsnek <kokrsnek@gmail.com>
2026-09-05 21:16:25 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -51,7 +51,7 @@ self.addEventListener('fetch', (event) => {
   const isHTML = event.request.destination === 'document' || url.pathname.endsWith('.html') || url.pathname === '/';
 
   if (isHTML) {
-    event.respondWith(fetch(event.request));
+    event.respondWith(fetch(event.request, { cache: 'no-store' }));
     return;
   }
 
